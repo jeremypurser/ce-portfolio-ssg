ca1148683191357eadcb2977d28d3746a8b041fb
Jeremy Purser <jeremypurser@gmail.com>
2020-09-07 18:17:12 -0400

    Add helper for about pages

--- src/lib/posts.js
+++ src/lib/posts.js
@@ -1,8 +1,10 @@
 import fs from 'fs';
 import matter from 'gray-matter';
+import fetch from 'isomorphic-unfetch';
 import path from 'path';
 import remark from 'remark';
 import html from 'remark-html';
+import { config } from '../config';
 
 const postsDirectory = path.join(process.cwd(), 'posts');
 
@@ -81,3 +83,54 @@ export async function getPostData(id) {
     ...matterResult.data,
   };
 }
+
+export async function getAllPageIds() {
+  const response = await fetch(`${config.api}/pages`, {
+    method: 'GET',
+  });
+
+  const json = await response.json();
+
+  return json.map(({ id }) => ({
+    params: {
+      id: id.toString(),
+    },
+  }));
+}
+
+export async function getPagesData(id) {
+  const response = await fetch(`${config.api}/pages/${id}`, {
+    method: 'GET',
+  });
+
+  const json = await response.json();
+
+  const processedContent = await remark().use(html).process(json.description);
+
+  const contentHtml = processedContent.toString();
+
+  return {
+    ...json,
+    description: contentHtml,
+  };
+}
+
+export async function getSortedPagesData() {
+  const response = await fetch(`${config.api}/pages`, {
+    method: 'GET',
+  });
+
+  const pagesData = await response.json();
+
+  const data = pagesData.map((page) => {
+    return {
+      id: page.id,
+      name: page.name,
+      order: page.order,
+    };
+  });
+
+  return data.sort((a, b) => a.order - b.order);
+}
+
+// TODO: move to `lib/about`
