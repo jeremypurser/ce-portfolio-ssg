5944db97c16f6b65d73ba9c36e271781d7164e49
Jeremy Purser <jeremypurser@gmail.com>
2020-09-08 07:16:24 -0400

    Use google font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
+import Head from 'next/head';
 import '../styles/global.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
