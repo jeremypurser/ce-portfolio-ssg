fcc85951159c86f4f5a427dbfeddc954305079ad
Jeremy Purser <jeremypurser@gmail.com>
2020-09-12 14:30:01 -0400

    Use slug for about

--- src/lib/about.js
+++ src/lib/about.js
@@ -3,33 +3,33 @@ import remark from 'remark';
 import html from 'remark-html';
 import { config } from '../config';
 
-export async function getAllPageIds() {
+export async function getPageSlugs() {
   const response = await fetch(`${config.api}/pages`, {
     method: 'GET',
   });
 
   const json = await response.json();
 
-  return json.map(({ id }) => ({
+  return json.map(({ slug }) => ({
     params: {
-      id: id.toString(),
+      slug,
     },
   }));
 }
 
-export async function getPagesData(id) {
-  const response = await fetch(`${config.api}/pages/${id}`, {
+export async function getPagesData(slug) {
+  const response = await fetch(`${config.api}/pages?slug=${slug}`, {
     method: 'GET',
   });
 
-  const json = await response.json();
+  const [page] = await response.json();
 
-  const processedContent = await remark().use(html).process(json.description);
+  const processedContent = await remark().use(html).process(page.description);
 
   const contentHtml = processedContent.toString();
 
   return {
-    ...json,
+    ...page,
     description: contentHtml,
   };
 }
@@ -41,13 +41,5 @@ export async function getSortedPagesData() {
 
   const pagesData = await response.json();
 
-  const data = pagesData.map((page) => {
-    return {
-      id: page.id,
-      name: page.name,
-      order: page.order,
-    };
-  });
-
-  return data.sort((a, b) => a.order - b.order);
+  return pagesData.sort((a, b) => a.order - b.order);
 }
