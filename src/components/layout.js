ac05531613d058f40fb93caee19792402d37085b
Jeremy Purser <jeremypurser@gmail.com>
2020-09-12 12:41:13 -0400

    Update style path

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import styles from '../styles/layout.module.css';
 import utilStyles from '../styles/utils.module.css';
-import styles from './layout.module.css';
 
 const name = 'Catherine Edgerton';
 export const siteTitle = name;
