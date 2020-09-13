a29fd6510e7987113eef723d9d4ca5254641afdb
Jeremy Purser <jeremypurser@gmail.com>
2020-09-13 11:40:05 -0400

    Add category helpers and dynamic routes

---
+++ src/lib/category.js
@@ -0,0 +1,36 @@
+import fetch from 'isomorphic-unfetch';
+import { config } from '../config';
+
+export async function getCategoryNames() {
+  const response = await fetch(`${config.api}/categories`, {
+    method: 'GET',
+  });
+
+  const json = await response.json();
+
+  return json.map(({ name }) => ({
+    params: {
+      name,
+    },
+  }));
+}
+
+export async function getSortedCategoriesData() {
+  const response = await fetch(`${config.api}/categories`, {
+    method: 'GET',
+  });
+
+  const json = await response.json();
+
+  return json.sort((a, b) => a.id - b.id);
+}
+
+export async function getCategoryData(name) {
+  const response = await fetch(`${config.api}/categories?name=${name}`, {
+    method: 'GET',
+  });
+
+  const [json] = await response.json();
+
+  return json;
+}
---
+++ src/pages/[name].js
@@ -0,0 +1,46 @@
+import Head from 'next/head';
+import Layout from '../components/layout';
+import { getSortedPagesData } from '../lib/about';
+import {
+  getCategoryData,
+  getCategoryNames,
+  getSortedCategoriesData,
+} from '../lib/category';
+
+export default function Category({
+  categoryData,
+  allPagesData,
+  allCategoriesData,
+}) {
+  return (
+    <Layout allPagesData={allPagesData} allCategoriesData={allCategoriesData}>
+      <Head>{categoryData.name}</Head>
+      <article>
+        <h1>{categoryData.name}</h1>
+      </article>
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  const paths = await getCategoryNames();
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const categoryData = await getCategoryData(params.name);
+  const allPagesData = await getSortedPagesData();
+  const allCategoriesData = await getSortedCategoriesData();
+
+  return {
+    props: {
+      categoryData,
+      allPagesData,
+      allCategoriesData,
+    },
+  };
+}
