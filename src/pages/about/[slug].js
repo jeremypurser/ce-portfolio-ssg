7a6548f2161547b2fd363ad4b97bf2bb1916eda8
Jeremy Purser <jeremypurser@gmail.com>
2020-09-13 13:44:13 -0400

    Adjust styles

--- src/pages/about/[slug].js
+++ src/pages/about/[slug].js
@@ -14,7 +14,7 @@ export default function Page({ pageData, allPagesData, allCategoriesData }) {
       <Head>
         <title>{pageData.name}</title>
       </Head>
-      <article>
+      <article className={utilStyles.copy}>
         <h1 className={utilStyles.headingXl}>{pageData.name}</h1>
         <div dangerouslySetInnerHTML={{ __html: pageData.description }} />
       </article>
